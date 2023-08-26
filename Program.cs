d2d9f72221427d6dc948bc8623ba26cb25c7d351
chapcs <benchapman003@gmail.com>
2023-08-26 07:56:06 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+abstract class Animal
+{
+    public abstract void MakeNoise();
+}
+
+class Hippo : Animal
+{
+    public override void MakeNoise()
+    {
+        Console.WriteLine("Grunt.");
+    }
+    public void Swim()
+    {
+        Console.WriteLine("Splash! I'm going for a swim!");
+    }
+}
+
