1c99696bc3e29bb89d14c215d33a8947a2f72494
chapcs <benchapman003@gmail.com>
2023-08-26 08:00:22 -0400

    Added Canine and wolf class

--- Program.cs
+++ Program.cs
@@ -23,3 +23,27 @@ class Hippo : Animal
     }
 }
 
+abstract class Canine : Animal
+{
+    public bool BelongsToPack { get; protected set; } = false;//why is this set equal to false
+}
+
+class Wolf : Canine
+{
+    public Wolf(bool belongsToPack)
+    {
+        BelongsToPack = belongsToPack;
+    }
+
+    public override void MakeNoise()
+    {
+        if (BelongsToPack)
+            Console.WriteLine("I'm in a pack.");
+        Console.WriteLine("Howwwlll!");
+    }
+
+    public void HuntInPack()
+    {
+
+    }
+}
